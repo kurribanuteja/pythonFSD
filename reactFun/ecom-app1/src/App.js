24fc7e0ed04428ca89b762eeee36410f8a110e8c
K.Banu Teja <you@example.com>
2022-08-19 15:07:53 +0530

    app

--- reactFun/ecom-app1/src/App.js
+++ reactFun/ecom-app1/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import "./App.css";
+export default function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="card-border">
+      <div className="border-1">
+        <img src="./imgapp/blazers.jpg" className="icon1" alt="" />
+        <img src="./imgapp/Heart.svg" className="icon2" alt="" />
+        <p className="font1">
+          <b>Black Fantasy Sweatshirt</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="font2">
+          Price <b>$200</b>
+        </p>
+        <div className="border-2">
+          <img src="./imgapp/cart.svg" className="icon3" alt="" />
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
-
-export default App;
